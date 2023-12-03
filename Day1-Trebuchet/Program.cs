655d4d62d0494cd8c51b3b4cafe59b302fe779a2
DeltaDizzy <EJBraswell@gmail.com>
2023-12-02 19:08:52 -0500

    initial upload

---
+++ Day1-Trebuchet/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+string[] rawLines = File.ReadAllLines(@"C:\Users\DeltaDizzy\Documents\DOTNET\Advent of Code\2023\Day1-Trebuchet\input.txt");
+Regex digitFinder = new Regex("\\d|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)");
+int sum = 0;
+foreach (string line in rawLines)
+{
+    var replacedLine = digitFinder.Replace(line.ToLower(), MakeNumeric);
+    var digits = replacedLine.ToCharArray().Where(IsDigit).Select(c => c);
+    sum += int.Parse($"{digits.First()}{digits.Last()}");
+}
+Console.WriteLine(sum);
+
+bool IsDigit(char c) {
+    return char.IsDigit(c);
+}
+
+string ConvertToNumeric(string digit) =>
+    digit switch {
+        "one" => "1",
+        "two" => "2",
+        "three" => "3",
+        "four" => "4",
+        "five" => "5",
+        "six" => "6",
+        "seven" => "7",
+        "eight" => "8",
+        "nine" => "9",
+        _ => digit
+    };
+
+string MakeNumeric(Match match) {
+    return ConvertToNumeric(match.Value);
+}
+/*string GetArrayAsString(string[] array) {
+    StringBuilder stringBuilder = new();
+    stringBuilder.Append("[ ");
+    foreach (string item in array)
+    {
+        stringBuilder.Append($"{item} ");
+    }
+    stringBuilder.Append("]");
+    return stringBuilder.ToString();
+}
+
+struct CalibrationValue(string digits, string leftNum, string rightNum) {
+
+    public override string ToString()
+    {
+        return $"CalibrationValue {{ digits = {digits}, leftNum = {leftNum}, rightNum = {rightNum} }}";
+    }
+
+    public int Value {
+        get { return int.Parse(leftNum + rightNum); }
+    }
+}*/
+
---
+++ Day2-CubeConundrum/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+Regex gameIdMatch = new Regex(@"(?<!Game)\d+(?=:)");
+Regex redMatch = new Regex(@"\d+(?= red)");
+Regex greenMatch = new Regex(@"\d+(?= green)");
+Regex blueMatch = new Regex(@"\d+(?= blue)");
+List<GameData> allGames = new();
+
+GameData ParseGameData(string toParse) {
+    int gameId = int.Parse(gameIdMatch.Match(toParse).Value);
+    return new GameData(gameId, ParseGameRounds(toParse));
+}
+
+List<GameRound> ParseGameRounds(string toParse) {
+    string[] rounds = toParse.Split(';');
+    GameRound[] gameRounds = new GameRound[rounds.Length];
+    for (int i = 0; i < rounds.Length; i++)
+    {   
+        string match = redMatch.Match(rounds[i]).Value;
+        int redNumber = int.Parse(match != "" ? match : "0");
+        match = greenMatch.Match(rounds[i]).Value;
+        int greenNumber = int.Parse(match != "" ? match : "0");
+        match = blueMatch.Match(rounds[i]).Value;
+        int blueNumber = int.Parse(match != "" ? match : "0");
+        gameRounds[i] = new GameRound(redNumber, greenNumber, blueNumber);
+    }
+    return gameRounds.ToList();
+}
+
+void LoadInput() {
+    // Load input
+    string[] gameText = File.ReadAllLines(@"C:\Users\DeltaDizzy\Documents\DOTNET\Advent of Code\2023\Day2-CubeConundrum\input.txt");
+    gameText.ToList().ForEach(s => allGames.Add(ParseGameData(s)));
+    Console.WriteLine("Games loaded");
+}
+
+void Part1() {
+    LoadInput();
+
+    // define max numbers
+    GameRound max = new GameRound(12, 13, 14);
+
+    // find possible games
+    List<GameData> possibleGames = allGames
+        .Where(game => game.rounds.Max(round => round.red) <= max.red)
+        .Where(game => game.rounds.Max(round => round.green) <= max.green)
+        .Where(game => game.rounds.Max(round => round.blue) <= max.blue)
+        .ToList();
+
+    //sum IDs
+    int possibleIdSum = possibleGames.Sum(game => game.Id);
+    Console.WriteLine(possibleIdSum);
+}
+
+void Part2() {
+    LoadInput();
+    // find required cube counts for each game
+    IEnumerable<GameRound> neededCubes = allGames.Select(game => game.MaxValues);
+    // find power for each game
+    IEnumerable<int> powers = neededCubes.Select(game => game.red * game.green * game.blue);
+    // sum powers
+    int sumOfPowers = powers.Sum();
+    Console.WriteLine(sumOfPowers);
+}
+
+Part2();
+
+record GameRound(int red, int green, int blue);
+record GameData(int Id, List<GameRound> rounds) {
+    public GameRound MaxValues => new GameRound(rounds.Max(round => round.red), rounds.Max(round => round.green), rounds.Max(round => round.blue));
+}
