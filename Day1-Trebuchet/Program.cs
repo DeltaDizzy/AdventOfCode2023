49a44edec54b22bed65fca7409408300f2c93df2
DeltaDizzy <EJBraswell@gmail.com>
2023-12-02 20:17:20 -0500

    day 1 part 2 done

--- Day1-Trebuchet/Program.cs
+++ Day1-Trebuchet/Program.cs
@@ -1,20 +1,16 @@
-﻿using System.Text;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 string[] rawLines = File.ReadAllLines(@"C:\Users\DeltaDizzy\Documents\DOTNET\Advent of Code\2023\Day1-Trebuchet\input.txt");
-Regex digitFinder = new Regex("\\d|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)");
+Regex digitFinder = new Regex(@"(?=(\d{1}|one|two|three|four|five|six|seven|eight|nine))");
 int sum = 0;
 foreach (string line in rawLines)
 {
-    var replacedLine = digitFinder.Replace(line.ToLower(), MakeNumeric);
-    var digits = replacedLine.ToCharArray().Where(IsDigit).Select(c => c);
-    sum += int.Parse($"{digits.First()}{digits.Last()}");
+    var matches = digitFinder.Matches(line);
+    string firstDigit = ConvertToNumeric(matches[0].Groups[1].Value);
+    string lastDigit = ConvertToNumeric(matches[^1].Groups[1].Value);
+    sum += int.Parse($"{firstDigit}{lastDigit}");
 }
 Console.WriteLine(sum);
-
-bool IsDigit(char c) {
-    return char.IsDigit(c);
-}
 
 string ConvertToNumeric(string digit) =>
     digit switch {
@@ -29,30 +25,3 @@ string ConvertToNumeric(string digit) =>
         "nine" => "9",
         _ => digit
     };
-
-string MakeNumeric(Match match) {
-    return ConvertToNumeric(match.Value);
-}
-/*string GetArrayAsString(string[] array) {
-    StringBuilder stringBuilder = new();
-    stringBuilder.Append("[ ");
-    foreach (string item in array)
-    {
-        stringBuilder.Append($"{item} ");
-    }
-    stringBuilder.Append("]");
-    return stringBuilder.ToString();
-}
-
-struct CalibrationValue(string digits, string leftNum, string rightNum) {
-
-    public override string ToString()
-    {
-        return $"CalibrationValue {{ digits = {digits}, leftNum = {leftNum}, rightNum = {rightNum} }}";
-    }
-
-    public int Value {
-        get { return int.Parse(leftNum + rightNum); }
-    }
-}*/
-
